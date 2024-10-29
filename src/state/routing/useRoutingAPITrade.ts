86292db9d0bb6ea29b68a2f3490aa05daa633204
Josh Dechant <josh@codecrafting.org>
2024-10-29 09:52:34 -0400

    lint

--- src/state/routing/useRoutingAPITrade.ts
+++ src/state/routing/useRoutingAPITrade.ts
@@ -1,7 +1,6 @@
 import { skipToken } from '@reduxjs/toolkit/query/react'
 import { Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
 import { sendTiming } from 'components/analytics'
-import { AVERAGE_L1_BLOCK_TIME } from 'constants/chainInfo'
 import { useStablecoinAmountFromFiatValue } from 'hooks/useStablecoinPrice'
 import { useRoutingAPIArguments } from 'lib/hooks/routing/useRoutingAPIArguments'
 import useIsValidBlock from 'lib/hooks/useIsValidBlock'
