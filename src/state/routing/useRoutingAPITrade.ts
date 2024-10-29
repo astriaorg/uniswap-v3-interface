e87888eb8f212114043e10dd7f04faac8e514e49
Josh Dechant <josh@codecrafting.org>
2024-10-29 09:48:55 -0400

    tone down refetching quotes

--- src/state/routing/useRoutingAPITrade.ts
+++ src/state/routing/useRoutingAPITrade.ts
@@ -46,7 +46,7 @@ export function useRoutingAPITrade<TTradeType extends TradeType>(
 
   const { isLoading, isError, data, currentData } = useGetQuoteQuery(queryArgs ?? skipToken, {
     // Price-fetching is informational and costly, so it's done less frequently.
-    pollingInterval: routerPreference === RouterPreference.PRICE ? ms`2m` : AVERAGE_L1_BLOCK_TIME,
+    pollingInterval: routerPreference === RouterPreference.PRICE ? ms`2m` : ms`10s`,
   })
 
   const quoteResult: GetQuoteResult | undefined = useIsValidBlock(Number(data?.blockNumber) || 0) ? data : undefined
